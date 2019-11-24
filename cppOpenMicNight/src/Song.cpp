34f0c4e51e63dcd9c347ba85b297e4873c2b96a0
Kyle <kgbostian@gmail.com>
2019-11-24 15:59:43 -0600

    More naming conventions enforced.

--- cppOpenMicNight/src/Song.cpp
+++ cppOpenMicNight/src/Song.cpp
@@ -1,4 +1,4 @@
-#include "song.h"
+#include "Song.h"
 Song::Song(std::string name)
 {
     this->name = name;
--- cppOpenMicNight/test/test_album.cpp
+++ cppOpenMicNight/test/test_album.cpp
@@ -1,8 +1,17 @@
-#include "album.h"
+#include "Album.h"
 #include "gtest/gtest.h"
 
 TEST(AlbumTests, DefaultAlbum)
 {
-    Album a = Album();
-    ASSERT_STREQ("", a.getName().c_str());
+    Album a = Album("New Album");
+    ASSERT_STREQ("New Album", a.getName().c_str());
+};
+
+TEST(AlbumTests, AddSongs)
+{
+    //Album a = Album();
+    //Song s = Song("Adding Song");
+    //a.addSong(s);
+    //std::list<Song> sl = a.getSongList();
+    //ASSERT_EQ(1,sl.size());
 };
